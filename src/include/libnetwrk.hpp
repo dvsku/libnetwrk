3d4ce17fe3f525b5602d0eda9020105ccd1d023e
dvsku <nikola.stojsin@hotmail.com>
2023-03-04 08:15:28 +0100

    Macros for only including parts of libnetwrk

--- src/include/libnetwrk.hpp
+++ src/include/libnetwrk.hpp
@@ -2,8 +2,39 @@
 #define LIBNETWRK_HPP
 
 #include "net/message.hpp"
-#include "net/tcp/tcp_connection.hpp"
-#include "net/tcp/tcp_server.hpp"
-#include "net/tcp/tcp_client.hpp"
+
+#if defined(LIBNETWRK_SERVER)
+	#if defined(LIBNETWRK_TCP)
+		#include "net/tcp/tcp_connection.hpp"
+		#include "net/tcp/tcp_server.hpp"
+	#elif defined(LIBNETWRK_UDP)
+
+	#else
+		#include "net/tcp/tcp_connection.hpp"
+		#include "net/tcp/tcp_server.hpp"
+	#endif
+#elif defined(LIBNETWRK_CLIENT)
+	#if defined(LIBNETWRK_TCP)
+		#include "net/tcp/tcp_connection.hpp"
+		#include "net/tcp/tcp_client.hpp"
+	#elif defined(LIBNETWRK_UDP)
+	
+	#else
+		#include "net/tcp/tcp_connection.hpp"
+		#include "net/tcp/tcp_client.hpp"
+	#endif
+#else
+	#if defined(LIBNETWRK_TCP)
+		#include "net/tcp/tcp_connection.hpp"
+		#include "net/tcp/tcp_server.hpp"
+		#include "net/tcp/tcp_client.hpp"
+	#elif defined(LIBNETWRK_UDP)
+
+	#else
+		#include "net/tcp/tcp_connection.hpp"
+		#include "net/tcp/tcp_server.hpp"
+		#include "net/tcp/tcp_client.hpp"
+	#endif
+#endif
 
 #endif
