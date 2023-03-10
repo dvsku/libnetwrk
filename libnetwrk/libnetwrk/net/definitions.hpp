e4eff4d7c704c6d566c3a62728aaa19df90ba4b1
dvsku <nikola.stojsin@hotmail.com>
2023-03-10 07:28:30 +0100

    Remove unnecessary definitions

--- libnetwrk/libnetwrk/net/definitions.hpp
+++ libnetwrk/libnetwrk/net/definitions.hpp
@@ -15,13 +15,6 @@ namespace libnetwrk {
 	typedef std::shared_ptr<asio::ip::tcp::acceptor> acceptor_ptr;
 }
 
-#define BUFFER_		std::vector
-#define BUFFER_U8	std::vector<uint8_t>
-#define BUFFER_U16	std::vector<uint16_t>
-#define BUFFER_U32	std::vector<uint32_t>
-#define BUFFER_U64	std::vector<uint64_t>
-#define BUFFER_STR	std::vector<std::string>
-
 // use to suppress unreferenced local variable warnings
 #define VAR_IGNORE(x) (void)x;
 
