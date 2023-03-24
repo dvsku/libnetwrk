6f9360ca3ad875444ab0eec4593c9630ffa89b47
dvsku <nikola.stojsin@hotmail.com>
2023-03-24 05:01:41 +0100

    tslist.hpp

--- test/utilities_assert.hpp
+++ test/utilities_assert.hpp
@@ -16,6 +16,21 @@
         assert(cpputExceptionThrown_ == true);                          \
         break;                                                          \
     } while (false);                                                    \
-}                                                                       \
+}
+
+#define ASSERT_NOT_THROWS(expression) {                                 \
+    do {                                                                \
+        bool cpputExceptionThrown_ = false;                             \
+        try {                                                           \
+            expression;                                                 \
+        }                                                               \
+        catch (...) {                                                   \
+            cpputExceptionThrown_ = true;                               \
+        }                                                               \
+                                                                        \
+        assert(cpputExceptionThrown_ == true);                          \
+        break;                                                          \
+    } while (false);                                                    \
+}
 
 #endif
