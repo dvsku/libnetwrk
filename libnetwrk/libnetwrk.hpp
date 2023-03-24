006b6db6354e1cba6ec6618e7b8d0cd281ddbc5a
dvsku <nikola.stojsin@hotmail.com>
2023-03-24 05:10:36 +0100

    Update libnetwrk.hpp

--- libnetwrk/libnetwrk.hpp
+++ libnetwrk/libnetwrk.hpp
@@ -1,43 +1,17 @@
 ﻿#ifndef LIBNETWRK_HPP
 #define LIBNETWRK_HPP
 
+#include "libnetwrk/utilities/log.hpp"
+#include "libnetwrk/net/definitions.hpp"
 #include "libnetwrk/net/message.hpp"
 #include "libnetwrk/net/common/containers/buffer.hpp"
-
+#include "libnetwrk/net/common/containers/tsdeque.hpp"
+#include "libnetwrk/net/common/containers/tslist.hpp"
+#include "libnetwrk/net/common/exceptions/libnetwrk_exception.hpp"
+#include "libnetwrk/net/common/serialization/serializable.hpp"
 #include "libnetwrk/net/common/serialization/serializers/binary_serializer.hpp"
-
-#if defined(LIBNETWRK_SERVER)
-	#if defined(LIBNETWRK_TCP)
-		#include "libnetwrk/net/tcp/tcp_connection.hpp"
-		#include "libnetwrk/net/tcp/tcp_server.hpp"
-	#elif defined(LIBNETWRK_UDP)
-
-	#else
-		#include "libnetwrk/net/tcp/tcp_connection.hpp"
-		#include "libnetwrk/net/tcp/tcp_server.hpp"
-	#endif
-#elif defined(LIBNETWRK_CLIENT)
-	#if defined(LIBNETWRK_TCP)
-		#include "libnetwrk/net/tcp/tcp_connection.hpp"
-		#include "libnetwrk/net/tcp/tcp_client.hpp"
-	#elif defined(LIBNETWRK_UDP)
-	
-	#else
-		#include "libnetwrk/net/tcp/tcp_connection.hpp"
-		#include "libnetwrk/net/tcp/tcp_client.hpp"
-	#endif
-#else
-	#if defined(LIBNETWRK_TCP)
-		#include "libnetwrk/net/tcp/tcp_connection.hpp"
-		#include "libnetwrk/net/tcp/tcp_server.hpp"
-		#include "libnetwrk/net/tcp/tcp_client.hpp"
-	#elif defined(LIBNETWRK_UDP)
-
-	#else
-		#include "libnetwrk/net/tcp/tcp_connection.hpp"
-		#include "libnetwrk/net/tcp/tcp_server.hpp"
-		#include "libnetwrk/net/tcp/tcp_client.hpp"
-	#endif
-#endif
+#include "libnetwrk/net/tcp/tcp_connection.hpp"
+#include "libnetwrk/net/tcp/tcp_server.hpp"
+#include "libnetwrk/net/tcp/tcp_client.hpp"
 
 #endif
