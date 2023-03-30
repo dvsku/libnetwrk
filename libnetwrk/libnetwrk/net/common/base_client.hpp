62369cf4c8ba03654530586c20fdd7dcd23320f2
dvsku <nikola.stojsin@hotmail.com>
2023-03-30 08:19:49 +0200

    Create base_client

---
+++ libnetwrk/libnetwrk/net/common/base_client.hpp
@@ -0,0 +1,186 @@
+#ifndef LIBNETWRK_NET_COMMON_BASE_CLIENT_HPP
+#define LIBNETWRK_NET_COMMON_BASE_CLIENT_HPP
+
+#include "libnetwrk/net/definitions.hpp"
+#include "libnetwrk/net/message.hpp"
+#include "libnetwrk/net/macros.hpp"
+#include "libnetwrk/net/common/base_connection.hpp"
+#include "libnetwrk/net/common/serialization/serializers/binary_serializer.hpp"
+#include "libnetwrk/net/common/containers/tsdeque.hpp"
+
+namespace libnetwrk::net::common {
+	template <typename command_type,
+		typename serializer = libnetwrk::net::common::binary_serializer,
+		typename storage = libnetwrk::nothing>
+	class base_client {
+		public:
+			typedef libnetwrk::net::message<command_type, serializer> message_t;
+			typedef std::shared_ptr<message_t> message_t_ptr;
+			typedef libnetwrk::net::owned_message<command_type, serializer, storage> owned_message_t;
+
+			typedef base_connection<command_type, serializer, storage> base_connection_t;
+			typedef std::shared_ptr<base_connection_t> base_connection_t_ptr;
+
+		protected:
+			std::string m_name;
+			bool m_connected = false;
+
+			context_ptr m_context;
+			libnetwrk::net::common::tsdeque<owned_message_t> m_incoming_messages;
+
+			std::thread m_context_thread;
+			std::thread m_process_messages_thread;
+
+			base_connection_t_ptr m_connection;
+
+		public:
+			base_client(const std::string& name = "base client") {
+				LIBNETWRK_STATIC_ASSERT_OR_THROW(std::is_enum<command_type>::value,
+					"client command_type template arg can only be an enum");
+				
+				m_name = name;
+
+				try {
+					m_context = std::make_shared<asio::io_context>(1);
+				}
+				catch (const std::exception& e) {
+					LIBNETWRK_ERROR("failed to create tcp_client | %s", e.what());
+				}
+			}
+
+			virtual ~base_client() {
+				stop();
+			}
+
+			/// <summary>
+			/// Client status
+			/// </summary>
+			/// <returns>true if connected, false if disconnected</returns>
+			bool connected() {
+				return m_connected;
+			}
+
+			/// <summary>
+			/// Stop the client and clean up
+			/// </summary>
+			void stop() {
+				m_connected = false;
+
+				if (m_context)
+					if (!m_context->stopped())
+						m_context->stop();
+
+				if (m_connection)
+					if (m_connection->is_alive())
+						m_connection->stop();
+
+				m_incoming_messages.cancel_wait();
+
+				if (m_context_thread.joinable())
+					m_context_thread.join();
+
+				if (m_process_messages_thread.joinable())
+					m_process_messages_thread.join();
+
+				LIBNETWRK_INFO("%s stopped", m_name.c_str());
+			}
+
+			/// <summary>
+			/// Connect to TCP server
+			/// </summary>
+			/// <param name="host">: IPv4 address</param>
+			/// <param name="port">: port</param>
+			/// <returns>true if connected, false if not</returns>
+			bool connect(const char* host, const unsigned short port) {
+				return _connect(host, port);
+			}
+
+			/// <summary>
+			/// Processes a single message if the queue is not empty.
+			/// </summary>
+			/// <returns>true if a message has been processed, false if it hasn't</returns>
+			bool process_single_message() {
+				try {
+					if (m_incoming_messages.empty())
+						return false;
+
+					message_t msg = m_incoming_messages.pop_front().m_msg;
+					on_message(msg);
+				}
+				catch (const std::exception& e) {
+					LIBNETWRK_ERROR("process_single_message() fail | %s", e.what());
+					return false;
+				}
+				catch (...) {
+					LIBNETWRK_ERROR("process_single_message() fail | undefined reason");
+					return false;
+				}
+
+				return true;
+			}
+
+			/// <summary>
+			/// Process messages while client is connected. This is a blocking function.
+			/// </summary>
+			void process_messages() {
+				_process_messages();
+			}
+
+			/// <summary>
+			/// Process messages while client is connected. 
+			/// This function runs asynchronously until the client stops.
+			/// </summary>
+			void process_messages_async() {
+				m_process_messages_thread = std::thread([&] { _process_messages(); });
+			}
+
+			/// <summary>
+			/// Send a message
+			/// </summary>
+			/// <param name="message">: message to send</param>
+			void send(message_t& message) {
+				if (m_connection && m_connected) {
+					if (m_connection->is_alive()) {
+						m_connection->send(std::make_shared<message_t>(std::move(message)));
+					}
+					else {
+						on_disconnect();
+						stop();
+					}
+				}
+			}
+
+		protected:
+			virtual void on_message(message_t& msg) {}
+
+			virtual void on_disconnect() {}
+
+			virtual bool _connect(const char* host, const unsigned short port) {
+				return false;
+			}
+
+		private:
+			void _process_messages(size_t max_messages = -1) {
+				while (m_connected) {
+					m_incoming_messages.wait();
+
+					try {
+						size_t message_count = 0;
+						while (message_count < max_messages && !m_incoming_messages.empty()) {
+							message_t msg = m_incoming_messages.pop_front().m_msg;
+							on_message(msg);
+							message_count++;
+						}
+					}
+					catch (const std::exception& e) {
+						LIBNETWRK_ERROR("do_process_messages() fail | %s", e.what());
+					}
+					catch (...) {
+						LIBNETWRK_ERROR("do_process_messages() fail | undefined reason");
+					}
+				}
+			}
+	};
+}
+
+#endif
--- libnetwrk/libnetwrk/net/tcp/tcp_client.hpp
+++ libnetwrk/libnetwrk/net/tcp/tcp_client.hpp
@@ -5,161 +5,45 @@
 #include <exception>
 #include <type_traits>
 
-#include "libnetwrk/net/message.hpp"
-#include "libnetwrk/net/common/containers/tsdeque.hpp"
-#include "libnetwrk/net/tcp/tcp_connection.hpp"
 #include "libnetwrk/net/definitions.hpp"
+#include "libnetwrk/net/message.hpp"
 #include "libnetwrk/net/macros.hpp"
+#include "libnetwrk/net/common/containers/tsdeque.hpp"
+#include "libnetwrk/net/common/base_client.hpp"
+#include "libnetwrk/net/tcp/tcp_connection.hpp"
 
 namespace libnetwrk::net::tcp {
 	template <typename command_type, 
 		typename serializer = libnetwrk::net::common::binary_serializer,
 		typename storage = libnetwrk::nothing>
-	class tcp_client {
+	class tcp_client 
+		: public libnetwrk::net::common::base_client<command_type, serializer, storage> 
+	{
 		public:
 			typedef libnetwrk::net::message<command_type, serializer> message_t;
 			typedef std::shared_ptr<message_t> message_t_ptr;
 			typedef libnetwrk::net::owned_message<command_type, serializer, storage> owned_message_t;
-
-			typedef tcp_connection<command_type, serializer, storage> tcp_connection_t;
-			typedef std::shared_ptr<tcp_connection_t> tcp_connection_t_ptr;
-
 			typedef command_type cmd_t;
 
-		protected:
-			tcp_connection_t_ptr m_connection;
-			context_ptr m_context;
-
-			libnetwrk::net::common::tsdeque<owned_message_t> m_incoming_messages;
-
-			std::thread m_context_thread;
-			std::thread m_process_messages_thread;
-
-			bool m_connected = false;
+		private:
+			typedef tcp_connection<command_type, serializer, storage> tcp_connection_t;
+			typedef std::shared_ptr<tcp_connection_t> tcp_connection_t_ptr;
+			typedef libnetwrk::net::common::base_client<command_type, serializer, storage> base;
 
 		public:
-			tcp_client() {
-				LIBNETWRK_STATIC_ASSERT_OR_THROW(std::is_enum<command_type>::value,
-					"client command_type template arg can only be an enum");
-
-				try {
-					m_context = std::make_shared<asio::io_context>(1);
-				}
-				catch (const std::exception& e) {
-					LIBNETWRK_ERROR("failed to create tcp_client | %s", e.what());
-				}
-			}
-
-			~tcp_client() {
-				stop();
-			}
-
-			/// <summary>
-			/// Client status
-			/// </summary>
-			/// <returns>true if connected, false if disconnected</returns>
-			bool connected() {
-				return m_connected;
-			}
-
-			/// <summary>
-			/// Connect to TCP server
-			/// </summary>
-			/// <param name="host">: IPv4 address</param>
-			/// <param name="port">: port</param>
-			/// <returns>true if connected, false if not</returns>
-			bool connect(const char* host, const unsigned short port) {
-				return _connect(host, port);
-			}
-
-			/// <summary>
-			/// Stop the client and clean up
-			/// </summary>
-			void stop() {
-				m_connected = false;
-
-				if (m_context)
-					if (!m_context->stopped())
-						m_context->stop();
-
-				if (m_connection)
-					if (m_connection->is_alive())
-						m_connection->stop();
-
-				m_incoming_messages.cancel_wait();
-
-				if (m_context_thread.joinable())
-					m_context_thread.join();
-
-				if (m_process_messages_thread.joinable())
-					m_process_messages_thread.join();
-
-				LIBNETWRK_INFO("tcp_client stopped");
-			}
-
-			/// <summary>
-			/// Processes a single message if the queue is not empty.
-			/// </summary>
-			/// <returns>true if a message has been processed, false if it hasn't</returns>
-			bool process_single_message() {
-				try {
-					if (m_incoming_messages.empty())
-						return false;
-
-					message_t msg = m_incoming_messages.pop_front().m_msg;
-					on_message(msg);
-				}
-				catch (const std::exception& e) {
-					LIBNETWRK_ERROR("process_single_message() fail | %s", e.what());
-					return false;
-				}
-				catch (...) {
-					LIBNETWRK_ERROR("process_single_message() fail | undefined reason");
-					return false;
-				}
-
-				return true;
-			}
-
-			/// <summary>
-			/// Process messages while client is connected. This is a blocking function.
-			/// </summary>
-			void process_messages() {
-				_process_messages();
-			}
-
-			/// <summary>
-			/// Process messages while client is connected. 
-			/// This function runs asynchronously until the client stops.
-			/// </summary>
-			void process_messages_async() {
-				m_process_messages_thread = std::thread([&] { _process_messages(); });
-			}
-
-			/// <summary>
-			/// Send a message
-			/// </summary>
-			/// <param name="message">: message to send</param>
-			void send(message_t& message) {
-				if (m_connection && m_connected) {
-					if (m_connection->is_alive()) {
-						m_connection->send(std::make_shared<message_t>(std::move(message)));
-					}
-					else {
-						on_disconnect();
-						stop();
-					}
-				}
-			}
+			tcp_client(const std::string& name = "tcp client") 
+				: libnetwrk::net::common::base_client<command_type, serializer, storage>(name) {}
+		
+			virtual ~tcp_client() {}
 
 		protected:
-			virtual void on_message(message_t& msg) { }
+			virtual void on_message(message_t& msg) override {}
 
-			virtual void on_disconnect() {}
+			virtual void on_disconnect() override {}
 
 		private:
-			bool _connect(const char* host, const unsigned short port) {
-				if (m_connected)
+			bool _connect(const char* host, const unsigned short port) override {
+				if (base::m_connected)
 					return false;
 
 				try {
@@ -167,59 +51,38 @@ namespace libnetwrk::net::tcp {
 					asio::ip::tcp::endpoint ep(asio::ip::address::from_string(host), port);
 
 					// Create ASIO socket
-					asio::ip::tcp::socket socket(*m_context, ep.protocol());
+					asio::ip::tcp::socket socket(*(base::m_context), ep.protocol());
 
 					// Connect
 					socket.connect(ep);
 
 					// Create connection object
-					m_connection = 
+					base::m_connection =
 						std::make_shared<tcp_connection_t>(libnetwrk::net::common::connection_owner::client,
-							std::move(socket), m_context, m_incoming_messages);
+							std::move(socket), base::m_context, base::m_incoming_messages);
 
 					// Start receiving messages
-					m_connection->start();
+					base::m_connection->start();
 
 					// Start ASIO context
-					m_context_thread = std::thread([&] { m_context->run(); });
+					base::m_context_thread = std::thread([&] { base::m_context->run(); });
 
-					m_connected = true;
+					base::m_connected = true;
 
 					LIBNETWRK_INFO("connected to %s:%d", host, port);
 				}
 				catch (const std::exception& e) {
 					LIBNETWRK_ERROR("failed to connect | %s", e.what());
-					stop();
+					base::stop();
 					return false;
 				}
 				catch (...) {
 					LIBNETWRK_ERROR("failed to connect | fatal error");
-					stop();
+					base::stop();
 					return false;
 				}
 
 				return true;
-			}
-
-			void _process_messages(size_t max_messages = -1) {
-				while (m_connected) {
-					m_incoming_messages.wait();
-
-					try {
-						size_t message_count = 0;
-						while (message_count < max_messages && !m_incoming_messages.empty()) {
-							message_t msg = m_incoming_messages.pop_front().m_msg;
-							on_message(msg);
-							message_count++;
-						}
-					}
-					catch (const std::exception& e) {
-						LIBNETWRK_ERROR("do_process_messages() fail | %s", e.what());
-					}
-					catch (...) {
-						LIBNETWRK_ERROR("do_process_messages() fail | undefined reason");
-					}
-				}
 			}
 	};
 }
