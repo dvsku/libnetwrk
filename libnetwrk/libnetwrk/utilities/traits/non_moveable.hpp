554229504ac4ef383a10ee4d451e6aafbd2be00f
dvsku <nikola.stojsin@hotmail.com>
2023-08-01 05:32:35 +0200

    Utility class traits

---
+++ libnetwrk/libnetwrk/utilities/traits/non_moveable.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace libnetwrk {
+	class non_moveable {
+	public:
+		non_moveable()					= default;
+		non_moveable(non_moveable&&)	= delete;
+		virtual ~non_moveable()			= default;
+		
+		non_moveable& operator= (non_moveable&&) = delete;
+	};
+}
