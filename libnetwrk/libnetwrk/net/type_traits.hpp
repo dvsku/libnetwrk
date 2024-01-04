fc55760a161e1448579a4be7eef8a009b64210d2
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-01-05 00:25:09 +0100

    Change custom object serialization

--- libnetwrk/libnetwrk/net/type_traits.hpp
+++ libnetwrk/libnetwrk/net/type_traits.hpp
@@ -1,10 +1,43 @@
 #pragma once
 
-#include "libnetwrk/net/core/serialization/serializable.hpp"
+#include <type_traits>
 
 namespace libnetwrk {
-    template<typename T, typename Tserialize>
-    concept is_serializable = std::is_base_of<libnetwrk::serializable<Tserialize>, T>::value;
+    // Forward decl buffer
+
+    template<typename Tserialize>
+    class buffer;
+
+    // Custom impl of std::convertable_to concept
+    // since not all compilers support it yet
+
+    template<typename From, typename To>
+    concept is_convertable_to = std::is_convertible_v<From, To> 
+        && requires { static_cast<To>(std::declval<From>()); };
+
+    // Check for existance of 
+    // template<typename T>
+    // void serialize(buffer<T>& buffer> const;
+
+    template <typename T, typename Tserialize>
+    concept has_serialize_func = requires(const T obj, buffer<Tserialize>&buf) {
+        { obj.serialize(buf) } -> is_convertable_to<void>;
+    };
+
+    // Check for existance of 
+    // template<typename T>
+    // void deserialize(buffer<T>& buffer>;
+
+    template <typename T, typename Tserialize>
+    concept has_deserialize_func = requires(T obj, buffer<Tserialize>&buf) {
+        { obj.deserialize(buf) } -> is_convertable_to<void>;
+        requires (!std::is_const_v<T>);
+    };
+
+    // Check if both serialize and deserialize exist
+
+    template <typename T, typename Tserialize>
+    concept is_serializable = has_serialize_func<T, Tserialize> && has_deserialize_func<T, Tserialize>;
 
     template<typename T>
     concept is_enum = std::is_enum<T>::value;
