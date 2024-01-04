3bc84998a23c73cb0ac448e94d2fefb361bf7de3
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-01-04 03:27:19 +0100

    Update libnetwrk.hpp

--- libnetwrk/libnetwrk.hpp
+++ libnetwrk/libnetwrk.hpp
@@ -1,15 +1,4 @@
-﻿#ifndef LIBNETWRK_HPP
-#define LIBNETWRK_HPP
+﻿#pragma once
 
-#include "libnetwrk/net/definitions.hpp"
-#include "libnetwrk/net/message.hpp"
-#include "libnetwrk/net/common/containers/buffer.hpp"
-#include "libnetwrk/net/common/containers/tsdeque.hpp"
-#include "libnetwrk/net/common/exceptions/libnetwrk_exception.hpp"
-#include "libnetwrk/net/common/serialization/serializable.hpp"
-#include "libnetwrk/net/common/serialization/serializers/binary_serializer.hpp"
-#include "libnetwrk/net/tcp/tcp_connection.hpp"
 #include "libnetwrk/net/tcp/tcp_server.hpp"
 #include "libnetwrk/net/tcp/tcp_client.hpp"
-
-#endif
