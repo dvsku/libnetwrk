efadc8029f9cdb13c43594fccd5554b4b2431c9c
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-01-04 02:00:08 +0100

    Refactor tcp_connection

--- libnetwrk/libnetwrk/net/tcp/tcp_connection.hpp
+++ libnetwrk/libnetwrk/net/tcp/tcp_connection.hpp
@@ -1,125 +1,118 @@
-#ifndef LIBNETWRK_NET_TCP_TCP_CONNECTION_HPP
-#define LIBNETWRK_NET_TCP_TCP_CONNECTION_HPP
+#pragma once
 
 #include "libnetwrk/net/common/base_connection.hpp"
 
-namespace libnetwrk::net::tcp {
-    template <typename command_type,
-        typename serializer,
-        typename storage = libnetwrk::nothing>
-    class tcp_connection 
-        : public libnetwrk::net::common::base_connection<command_type, serializer, storage>
-    {
-        public:
-            typedef libnetwrk::net::common::base_connection<command_type, serializer, storage> base_t;
+namespace libnetwrk::tcp {
+    template<typename Tcommand, typename Tserialize, typename Tstorage>
+    class tcp_connection : public libnetwrk::base_connection<Tcommand, Tserialize, Tstorage> {
+    public:
+        using base_t          = libnetwrk::base_connection<Tcommand, Tserialize, Tstorage>;
+        using base_context_t  = base_t::base_context_t;
+        using owned_message_t = base_t::owned_message_t;
+        using socket_t        = asio::ip::tcp::socket;
 
-            typedef base_t::owned_message_t        owned_message_t;
-            typedef base_t::base_context_t        base_context_t;
+    public:
+        tcp_connection(base_context_t& context, socket_t socket)
+            : base_t(context), m_socket(std::move(socket)) {}
 
-        protected:
-            asio::ip::tcp::socket m_socket;
+        /// <summary>
+        /// Get IPv4 address
+        /// </summary>
+        const std::string remote_address() override {
+            return m_socket.remote_endpoint().address().to_string();
+        }
 
-        public:
-            tcp_connection(base_context_t& parent_context, asio::ip::tcp::socket socket)
-                : base_t(parent_context), m_socket(std::move(socket)) {}
+        /// <summary>
+        /// Get port
+        /// </summary>
+        const unsigned short remote_port() override {
+            return m_socket.remote_endpoint().port();
+        }
 
-            /// <summary>
-            /// Get IPv4 address
-            /// </summary>
-            const std::string remote_address() override {
-                return m_socket.remote_endpoint().address().to_string();
-            }
+        /// <summary>
+        /// Close connection
+        /// </summary>
+        void stop() override {
+            m_socket.close();
+        }
 
-            /// <summary>
-            /// Get port
-            /// </summary>
-            const unsigned short remote_port() override {
-                return m_socket.remote_endpoint().port();
-            }
+        /// <summary>
+        /// Get connection status
+        /// </summary>
+        /// <returns>true if open, false if closed</returns>
+        bool is_alive() override {
+            return m_socket.is_open();
+        }
 
-            /// <summary>
-            /// Close connection
-            /// </summary>
-            void stop() override {
-                m_socket.close();
-            }
+    protected:
+        socket_t m_socket;
 
-            /// <summary>
-            /// Get connection status
-            /// </summary>
-            /// <returns>true if open, false if closed</returns>
-            bool is_alive() override {
-                return m_socket.is_open();
-            }
-
-        protected:
-            void read_verification_message() override {
-                if (this->m_parent_context.m_owner == connection_owner::server) {
-                    asio::async_read(m_socket, asio::buffer(&this->m_verification_code, sizeof(uint32_t)),
-                        std::bind(&tcp_connection::read_verification_message_callback,
-                            this, std::placeholders::_1, std::placeholders::_2));
-                }
-                else {
-                    std::error_code ec;
-                    size_t written = asio::read(m_socket,
-                        asio::buffer(&this->m_verification_code, sizeof(uint32_t)), ec);
-                    this->read_verification_message_callback(ec, written);
-                }
-            }
-
-            void read_message_head() override {
-                base_t::m_temp_message.m_head_data.reset_read_offset();
-
-                asio::async_read(m_socket, 
-                    asio::buffer(this->m_temp_message.m_head_data.data(),
-                        this->m_temp_message.m_head.size()),
-                    std::bind(&tcp_connection::read_message_head_callback, 
+    protected:
+        void read_verification_message() override {
+            if (this->m_context.m_owner == connection_owner::server) {
+                asio::async_read(m_socket, asio::buffer(&this->m_verification_code, sizeof(uint32_t)),
+                    std::bind(&tcp_connection::read_verification_message_callback,
                         this, std::placeholders::_1, std::placeholders::_2));
             }
+            else {
+                std::error_code ec;
+                size_t written = asio::read(m_socket,
+                    asio::buffer(&this->m_verification_code, sizeof(uint32_t)), ec);
+                this->read_verification_message_callback(ec, written);
+            }
+        }
 
-            void read_message_body() override {
-                asio::async_read(m_socket, asio::buffer(this->m_temp_message.m_data.data(), 
-                    this->m_temp_message.m_head.m_data_len),
-                    std::bind(&tcp_connection::read_message_body_callback, 
+        void read_message_head() override {
+            this->m_recv_message.data_head.reset_read_offset();
+
+            asio::async_read(m_socket,
+                asio::buffer(this->m_recv_message.data_head.data(),
+                    this->m_recv_message.head.size()),
+                std::bind(&tcp_connection::read_message_head_callback,
+                    this, std::placeholders::_1, std::placeholders::_2));
+        }
+
+        void read_message_body() override {
+            asio::async_read(m_socket, asio::buffer(this->m_recv_message.data.data(),
+                this->m_recv_message.head.data_size),
+                std::bind(&tcp_connection::read_message_body_callback,
+                    this, std::placeholders::_1, std::placeholders::_2));
+        }
+
+        void write_verification_message() override {
+            if (this->m_context.m_owner == connection_owner::server) {
+                asio::async_write(m_socket, asio::buffer(&this->m_verification_code, sizeof(uint32_t)),
+                    std::bind(&tcp_connection::write_verification_message_callback,
                         this, std::placeholders::_1, std::placeholders::_2));
             }
-
-            void write_verification_message() override {
-                if (this->m_parent_context.m_owner == connection_owner::server) {
-                    asio::async_write(m_socket, asio::buffer(&this->m_verification_code, sizeof(uint32_t)),
-                        std::bind(&tcp_connection::write_verification_message_callback,
-                            this, std::placeholders::_1, std::placeholders::_2));
-                }
-                else {
-                    std::error_code ec;
-                    size_t written = asio::write(m_socket, 
-                        asio::buffer(&this->m_verification_code, sizeof(uint32_t)), ec);
-                    this->write_verification_message_callback(ec, written);
-                }
+            else {
+                std::error_code ec;
+                size_t written = asio::write(m_socket,
+                    asio::buffer(&this->m_verification_code, sizeof(uint32_t)), ec);
+                this->write_verification_message_callback(ec, written);
             }
+        }
 
-            void write_message_head() override {
-                this->m_outgoing_messages.front()->m_head.m_send_timestamp =
-                    std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+        void write_message_head() override {
+            this->m_outgoing_messages.front()->head.send_timestamp =
+                std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 
-                this->m_outgoing_messages.front()->m_head_data.clear();
-                this->m_outgoing_messages.front()->m_head.serialize(this->m_outgoing_messages.front()->m_head_data);
-                
-                asio::async_write(m_socket, 
-                    asio::buffer(this->m_outgoing_messages.front()->m_head_data.data(),
-                        this->m_outgoing_messages.front()->m_head_data.size()),
-                    std::bind(&tcp_connection::write_message_head_callback, 
-                        this, std::placeholders::_1, std::placeholders::_2));
-            }
+            this->m_outgoing_messages.front()->data_head.clear();
+            this->m_outgoing_messages.front()->head.serialize(this->m_outgoing_messages.front()->data_head);
 
-            void write_message_body() override {
-                asio::async_write(m_socket, 
-                    asio::buffer(this->m_outgoing_messages.front()->m_data.data(),
-                        this->m_outgoing_messages.front()->m_data.size()),
-                    std::bind(&tcp_connection::write_message_body_callback, 
-                        this, std::placeholders::_1, std::placeholders::_2));
-            }
+            asio::async_write(m_socket,
+                asio::buffer(this->m_outgoing_messages.front()->data_head.data(),
+                    this->m_outgoing_messages.front()->data_head.size()),
+                std::bind(&tcp_connection::write_message_head_callback,
+                    this, std::placeholders::_1, std::placeholders::_2));
+        }
+
+        void write_message_body() override {
+            asio::async_write(m_socket,
+                asio::buffer(this->m_outgoing_messages.front()->data.data(),
+                    this->m_outgoing_messages.front()->data.size()),
+                std::bind(&tcp_connection::write_message_body_callback,
+                    this, std::placeholders::_1, std::placeholders::_2));
+        }
     };
 }
-
-#endif
