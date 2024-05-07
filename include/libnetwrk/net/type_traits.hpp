8bfada480d6f2b4df9035e1f3352ad92b5d01861
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-05-07 21:40:30 +0200

    Update spelling and formatting

--- include/libnetwrk/net/type_traits.hpp
+++ include/libnetwrk/net/type_traits.hpp
@@ -3,39 +3,40 @@
 #include <type_traits>
 
 namespace libnetwrk {
-    // Forward decl buffer
-
     template<typename Serialize>
     class buffer;
 
-    // Custom impl of std::convertable_to concept
-    // since not all compilers support it yet
-
+    /*
+        Custom impl of std::convertible_to concept since not all compilers support it yet
+    */
     template<typename From, typename To>
-    concept is_convertable_to = std::is_convertible_v<From, To> 
+    concept is_convertible_to = std::is_convertible_v<From, To>
         && requires { static_cast<To>(std::declval<From>()); };
 
-    // Check for existance of 
-    // template<typename T>
-    // void serialize(buffer<T>& buffer> const;
-
+    /*
+        Check for existence of
+        template<typename T>
+        void serialize(buffer<T>& buffer> const;
+    */
     template<typename T, typename Serialize>
     concept has_serialize_func = requires(const T obj, buffer<Serialize>&buf) {
-        { obj.serialize(buf) } -> is_convertable_to<void>;
+        { obj.serialize(buf) } -> is_convertible_to<void>;
     };
 
-    // Check for existance of 
-    // template<typename T>
-    // void deserialize(buffer<T>& buffer>;
-
+    /*
+        Check for existence of
+        template<typename T>
+        void deserialize(buffer<T>& buffer>;
+    */
     template<typename T, typename Serialize>
     concept has_deserialize_func = requires(T obj, buffer<Serialize>&buf) {
-        { obj.deserialize(buf) } -> is_convertable_to<void>;
+        { obj.deserialize(buf) } -> is_convertible_to<void>;
         requires (!std::is_const_v<T>);
     };
 
-    // Check if both serialize and deserialize exist
-
+    /*
+        Check if both serialize and deserialize exist
+    */
     template<typename T, typename Serialize>
     concept is_serializable = has_serialize_func<T, Serialize> && has_deserialize_func<T, Serialize>;
 
