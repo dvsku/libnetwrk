a9c95751f6c7c99272c5d553748054c99b842d5b
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-05-19 15:16:12 +0200

    Update example logging

--- examples/example_logging.hpp
+++ examples/example_logging.hpp
@@ -11,10 +11,10 @@
 #define LIBNETWRK_ERROR(component, frmt, ...)       \
     dvsku::dv_util_log::log_message(dvsku::dv_util_log::level::error,         component, frmt, ##__VA_ARGS__)
 
-#define LIBNETWRK_DEBUG(component, frmt, ...)       \
-    dvsku::dv_util_log::log_message(dvsku::dv_util_log::level::debug,         component, frmt, ##__VA_ARGS__)
-
 #define LIBNETWRK_VERBOSE(component, frmt, ...)     \
     dvsku::dv_util_log::log_message(dvsku::dv_util_log::level::verbose,       component, frmt, ##__VA_ARGS__)
+
+#define LIBNETWRK_DEBUG(component, frmt, ...)       \
+    dvsku::dv_util_log::log_message(dvsku::dv_util_log::level::debug,         component, frmt, ##__VA_ARGS__)
 
 #include <libnetwrk.hpp>
