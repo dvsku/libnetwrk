001ace446165dc4cea0c6dccf44ba817cdf4c8aa
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-05-30 17:56:50 +0200

    Change enums to enum classes

--- include/libnetwrk/net/enum/enums.hpp
+++ include/libnetwrk/net/enum/enums.hpp
@@ -5,22 +5,19 @@
 #include <cstdint>
 
 namespace libnetwrk {
-    #define LIBNETWRK_SET_FLAG(_flags, _flag) _flags =| _flag
-    #define LIBNETWRK_FLAG_SET(_flags, _flag) _flags  & _flag
-
-    enum service_status : uint8_t {
+    enum class service_status : uint8_t {
         stopped  = 0,
         starting = 1,
         started  = 2,
         stopping = 3
     };
 
-    enum send_flags : uint8_t {
+    enum class send_flags : uint8_t {
         none         = 0,
         keep_message = 1 << 0       // Make message reusable (copy instead of move upon sending)
     };
 
-    enum disconnect_code : uint8_t {
+    enum class disconnect_code : uint8_t {
         unspecified           = 0,
         authentication_failed = 1
     };
