6e1883b7406af0f7af5d36530c3a241a504af180
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-07-11 09:22:53 +0200

    Remove native_resolver_t heap alloc

--- include/libnetwrk/net/tcp/tcp_resolver.hpp
+++ include/libnetwrk/net/tcp/tcp_resolver.hpp
@@ -7,33 +7,23 @@
 namespace libnetwrk {
     class tcp_resolver {
     public:
-        // Context type for this resolver
-        using context_t = asio::io_context;
-
-        // Endpoint type for this resolver
+        using context_t  = asio::io_context;
         using endpoint_t = asio::ip::tcp::endpoint;
-    
-    private:
-        using native_resolver_t = asio::ip::tcp::resolver;
-    
+
     public:
         tcp_resolver()                    = delete;
         tcp_resolver(const tcp_resolver&) = delete;
         tcp_resolver(tcp_resolver&&)      = delete;
 
         tcp_resolver(context_t& context)
-            : m_context(context)
-        {
-            m_native = std::make_unique<native_resolver_t>(context);
-        }
+            : m_context(context), m_native(context)
+        {}
 
         tcp_resolver& operator=(const tcp_resolver&) = delete;
         tcp_resolver& operator=(tcp_resolver&&)      = delete;
 
     public:
         bool get_endpoint(const std::string& host, uint16_t port, endpoint_t& out_endpoint) {
-            if (!m_native) return false;
-
             static const std::regex pattern("([a-zA-Z0-9]+):\\/\\/([a-zA-Z0-9.-]+)\\/*");
 
             std::string l_host = "";
@@ -43,7 +33,7 @@ namespace libnetwrk {
                 if (matches.size() != 3) return false;
 
                 asio::ip::tcp::resolver::query        query(matches[2], matches[1]);
-                asio::ip::tcp::resolver::results_type endpoints = m_native->resolve(query);
+                asio::ip::tcp::resolver::results_type endpoints = m_native.resolve(query);
 
                 for (const auto& endpoint : endpoints) {
                     if (endpoint.endpoint().protocol() == asio::ip::tcp::v4()) {
@@ -73,7 +63,10 @@ namespace libnetwrk {
         }
 
     private:
-        context_t&                         m_context;
-        std::unique_ptr<native_resolver_t> m_native = nullptr;
+        using native_resolver_t = asio::ip::tcp::resolver;
+
+    private:
+        context_t&        m_context;
+        native_resolver_t m_native;
     };
 }
